f01089a22689a5c8801809a2580d1fcca48fa085
Sluchaino <145386647+Sluchaino@users.noreply.github.com>
2024-04-25 15:52:17 +0300

    Initial commit

---
+++ Laba2/Version2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+class Program
+{
+    static async Task Main()
+    {
+        Stopwatch time = new();
+        time.Start();
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "MyApp");
+
+        // Запрос вакансий по ключевому слову "developer"
+        HttpResponseMessage response = await client.GetAsync("https://api.hh.ru/vacancies?text=developer");
+        string json = await response.Content.ReadAsStringAsync();
+        File.WriteAllText("C://Users//Maks//Desktop//OOP//Laba2//json//async//vacancies_developer.json", json);
+        // Сохраняем ответ в файл
+
+        // Запрос вакансий по городу Москва
+        HttpResponseMessage response1 = await client.GetAsync("https://api.hh.ru/vacancies?area=1");
+        string json1 = await response1.Content.ReadAsStringAsync();
+        File.WriteAllText("C://Users//Maks//Desktop//OOP//Laba2//json//sync//moscow.json", json1);
+        // Сохраняем ответ в файл
+
+        // Запрос вакансий по компании "Альфа Банк"
+        HttpResponseMessage response2 = await client.GetAsync("https://api.hh.ru/vacancies?employer_id=80");
+        string json2 = await response2.Content.ReadAsStringAsync();
+        File.WriteAllText("C://Users//Maks//Desktop//OOP//Laba2//json//async//alfa_bank_async.json", json2);
+        // Сохраняем ответ в файл
+
+        time.Stop();
+        Console.WriteLine("Общее время работы: " + time.ElapsedMilliseconds);
+    }
+}
